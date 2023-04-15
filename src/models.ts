394133741a54d5ee62f702872c217be79936a739
Itay Fintz <itayfi@gmail.com>
2023-04-16 01:11:40 +0300

    Import models as assets

--- src/models.ts
+++ src/models.ts
@@ -1,12 +1,13 @@
 import Model, { ModelData } from "./Model";
 
-const models = import.meta.glob('./models/*.json');
+const models = import.meta.glob('./models/*.json', {as: 'url'});
 
 export function getModelNames() {
     return Object.keys(models).map(key => key.match(/([^/]*)\.json/)[1]);
 }
 
 export async function getModel(name: string): Promise<Model> {
-    const data = await models[`./models/${name}.json`]() as { default: ModelData };
-    return new Model(data.default as ModelData);
+    const respose = await fetch(await models[`./models/${name}.json`]() as string);
+    const data = await respose.json() as ModelData;
+    return new Model(data);
 }
